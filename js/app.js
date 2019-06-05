d0c5e2f8b4a9c42d24c2e87ef33b93d2e6f7918f
Manish KC <kcmanish89@gmail.com>
2019-06-04 18:24:13 -0700

    commit for the second lab02

--- js/app.js
+++ js/app.js
@@ -1,21 +1,67 @@
 'use strict';
 
 
-//Array demo
-//Array is a collection
-//Compared to java  '{}' it uses '[]'
-/*
-We access items in an array with bracket notation
+//Welcome Screen
+alert('Welcome to Guessing-Game! Respond with yes/no. Click OK to continue');
+var counter = 0; // counter to track the number of correct responses
 
-*/
+var firstGuess = prompt('Am I 35 years old?');
+console.log(firstGuess);
+if (firstGuess.toLowerCase() === 'yes' || firstGuess.toLowerCase() === 'y') {
+  alert('Omg! I am much younger!');
+} else if (firstGuess.toLowerCase() === 'no' || firstGuess.toLowerCase() === 'n') {
+  alert('I know, I look young! I am 29');
+  counter++;
+} else {
+  alert('Invalid Input!');
+}
 
-var emptyArray = [];
-var quizAnswers = ['a', 'b', 'c', 'c', 'e'];
-var arrayofThings = ['string', {}, ['thing'], console.log, undefined, null, 0, 2, true, false];
-console.log(arrayofThings);
+var secondGuess = prompt('Do I like chocolates?');
+console.log(secondGuess);
+if (secondGuess.toLowerCase() === 'yes' || secondGuess.toLowerCase() === 'y') {
+  alert('Right answer. Not sure if anybody dislikes chocolates!');
+  counter++;
+} else if (secondGuess.toLowerCase() === 'no' || secondGuess.toLowerCase() === 'n') {
+  alert('Wow! You got that so wrong!');
+} else {
+  alert('Invalid Input!');
+}
 
-console.log("test");
-console.log(quizAnswers);
-console.log(quizAnswers[2] === 'b');
-quizAnswers[2] = 'MANISH';
-console.log(quizAnswers);
+var thirdGuess = prompt('Do you think I own a cat?');
+console.log(thirdGuess);
+if (thirdGuess.toLowerCase() === 'yes' || thirdGuess.toLowerCase() === 'y') {
+  alert('Well, you must have mistaken me with someone else.');
+} else if (thirdGuess.toLowerCase() === 'no' || thirdGuess.toLowerCase() === 'n') {
+  alert('Bingo! I am a dog guy :)');
+  counter++;
+} else {
+  alert('Invalid Input!');
+}
+
+var fourthGuess = prompt('Do you think I like programming?');
+console.log(fourthGuess);
+if (fourthGuess.toLowerCase() === 'yes' || fourthGuess.toLowerCase() === 'y') {
+  alert('Yes, I love programming!');
+  counter++;
+} else if (fourthGuess.toLowerCase() === 'no' || fourthGuess.toLowerCase() === 'n') {
+  alert('Are we talking about you?');
+} else {
+  alert('Invalid Input!');
+}
+
+var fifthGuess = prompt('Do I play video games?');
+console.log(fifthGuess);
+if (fifthGuess.toLowerCase() === 'yes' || fifthGuess.toLowerCase() === 'y') {
+  alert('True. But, the only game I play is fifa. You should try it!');
+  counter++;
+} else if (fifthGuess.toLowerCase() === 'no' || fifthGuess.toLowerCase() === 'n') {
+  alert('We need to play sometime. ');
+} else {
+  alert('Invalid Input!');
+}
+
+if (counter < 5) {
+  alert('You got '+ counter + ' out of 5correct! Learn more about me in this page. ');
+} else {
+  alert('Wow! You got 5 out of 5 correct. Keep visiting the page!');
+}
